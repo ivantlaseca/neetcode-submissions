cd31b6871a845840611e2ff71fb5bca1b3e607ea
ivantlaseca <57382662+ivantlaseca@users.noreply.github.com>
2026-08-14 09:18:09 -0400

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.py
@@ -0,0 +1,49 @@
+"""
+} : {
+] : 
+)
+
+(}, false
+
+Algo:
+Map closeToOpen
+Iterate s, appending opening parentheses to stack and checking if closing parentheses correspond to top of stack
+
+{}, true
+
+{), false
+
+[(}], false
+"""
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) == 1:
+            return False
+        
+        closeToOpen = {
+            "}" : "{",
+            "]" : "[",
+            ")" : "("
+        }
+        openParentheses = []
+
+        for c in s:
+            isClosingBracket = c in closeToOpen
+            if isClosingBracket:
+                if openParentheses:
+                    isMismatch = closeToOpen[c] != openParentheses[-1]
+                    if isMismatch:
+                        return False
+                    else:
+                        openParentheses.pop()
+                else:
+                    return False
+            else:
+                openParentheses.append(c)
+        
+        return True if not openParentheses else False
+                
+                    
+        
